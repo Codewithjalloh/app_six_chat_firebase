a2be94cbcabbb0c72815945a0bcc4a6095484654
codewithjalloh <codewithjalloh@gmail.com>
2024-05-22 22:23:30 +0100

    home added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,13 @@
+import 'package:app_six_chat_firebase/services/auth/auth_service.dart';
+import 'package:app_six_chat_firebase/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  //
+  // chat and auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
