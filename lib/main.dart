05b6fb99f3231595d9a6643bfad9aa4d4ff9ff36
codewithjalloh <codewithjalloh@gmail.com>
2024-05-22 16:31:26 +0100

    chat service added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'services/auth/auth_gate.dart';
 import 'themes/theme_provider.dart';
 
 void main() async {
@@ -22,6 +23,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthGate(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
   }
 }
--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dark_mode.dart';
+import 'light_mode.dart';
+
 class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
 
