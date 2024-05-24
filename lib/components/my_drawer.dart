3a051b4c80fef3b2fec6daccac835f57a2256a43
codewithjalloh <codewithjalloh@gmail.com>
2024-05-24 08:38:24 +0100

    home list tile added

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -19,17 +19,31 @@ class MyDrawer extends StatelessWidget {
         children: [
           // logo
           Theme(
-              data: Theme.of(context).copyWith(
-                  dividerTheme: DividerThemeData(color: Colors.transparent)),
-              child: DrawerHeader(
-                child: Center(
-                  child: Icon(
-                    Icons.message,
-                    color: Theme.of(context).colorScheme.primary,
-                    size: 48,
-                  ),
+            data: Theme.of(context).copyWith(
+                dividerTheme: DividerThemeData(color: Colors.transparent)),
+            child: DrawerHeader(
+              child: Center(
+                child: Icon(
+                  Icons.message,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 48,
                 ),
-              ))
+              ),
+            ),
+          ),
+
+          // home list tile
+          Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              title: Text("H 0 M E"),
+              leading: Icon(Icons.home),
+              onTap: () {
+                // pop the nav
+                Navigator.pop(context);
+              },
+            ),
+          )
         ],
       ),
     );
