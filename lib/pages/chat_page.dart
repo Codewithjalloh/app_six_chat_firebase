b41053186177b05feb0b0d0dbb4a21525c3107f0
codewithjalloh <codewithjalloh@gmail.com>
2024-05-25 15:39:06 +0100

    chat page updated

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,7 @@
 import 'package:app_six_chat_firebase/services/auth/auth_service.dart';
 import 'package:app_six_chat_firebase/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -26,7 +28,6 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     // add listerner to focus node
@@ -73,6 +74,79 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: Text(widget.receivingEmail),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          // display all messages
+          Expanded(
+            child: _buildMessageList(),
+          ),
+          // user input
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+        stream: _chatService.getMessages(widget.receiverID, senderID),
+        builder: (context, snapshot) {
+          // errors
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading...");
+          }
+          // return list view
+          return ListView(
+            controller: _scrollController,
+            children: snapshot.data!.docs
+                .map((doc) => _buildMessageItem(doc))
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    // is current user
+    bool isCurrentUser = data["senderID"] == _authService.getCurrentUser()!.uid;
+
+    // alight messafe to the right send is current otherwise left
+    var alignment =
+        isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Column(
+        crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          ChatBubble(
+            message: data["message"],
+            isCurrentUser: isCurrentUser,
+          ),
+        ],
+      ),
+    )
+  }
+
+  // build message input
+  Widget _buildUserInput() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 50.0),
+    );
   }
 }
